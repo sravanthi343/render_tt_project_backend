9f9756fa166abd269868cf3ae6e06734c0e16c0c
sravanthi343 <sandelasravanthi343@gmail.com>
2026-04-05 20:14:54 +0530

    Update SecurityConfig.java

--- src/main/java/com/cms/config/SecurityConfig.java
+++ src/main/java/com/cms/config/SecurityConfig.java
@@ -37,11 +37,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsSource() {
         var config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://cms-project-frontend.onrender.com"));
        //config.setAllowedOriginPatterns(List.of("https://cms-project-frontend.onrender.com")); // TODO: Restrict to frontend URL in production
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://cms-project-frontend.onrender.com"));
+       // config.setAllowedOrigins(List.of("https://cms-project-frontend.onrender.com"));
         var source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
